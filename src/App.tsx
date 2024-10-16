a665fdbc050824a318d6bfc0072edf6cedb2df87
Mateus <118356038+Matz2022@users.noreply.github.com>
2024-10-16 08:06:33 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,25 @@ type ProdutoType = {
 }
 function App() {
   const [produtos, setProdutos] = useState<ProdutoType[]>([])
-  //useEffect (O que fazer, Quando Fazer)
+  //useEffect(O QUe fazer, Quando Fazer)
   useEffect(()=>{
     fetch("https://one022b-marketplace-fork-back-end.onrender.com/produtos")
     .then(resposta=>resposta.json())
     .then(dados=>setProdutos(dados))
   },[])
   return (
-    <>
+    <>  
       <div className="container-produtos">
-      {
-        produtos.map(prod=>{
-            return(
-              <div key={prod.id} className="produto-item">
-                <h1>{prod.nome}</h1>
-                <p>{prod.preco}</p>
-                <p>{prod.imagem}</p>
-                <p>`{prod.descricao}</p>
-              </div>
-            )
-        })
-      }
+        {produtos.map(prod=>{
+          return(
+            <div key={prod.id} className="produto-item">
+              <h1>{prod.nome}</h1>
+              <img src={prod.imagem} alt="Imagem de celular" />
+              <p>{prod.preco}</p>
+              <p>{prod.descricao}</p>
+            </div>
+          )
+        })}
       </div>
       
     </>
